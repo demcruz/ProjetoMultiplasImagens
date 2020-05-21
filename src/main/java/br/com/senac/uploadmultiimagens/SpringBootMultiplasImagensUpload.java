dfa9801b513764a7ad32527c7301d5cbfe69aeb6
Diego Cruz <diego-mcruz@hotmail.com>
2020-05-21 17:34:32 -0300

    Projeto Spring upload multilas imagens

---
+++ src/main/java/br/com/senac/uploadmultiimagens/SpringBootMultiplasImagensUpload.java
@@ -0,0 +1,30 @@
+package br.com.senac.uploadmultiimagens;
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = { 
+		SpringBootMultiplasImagensUpload.class,
+		Jsr310JpaConverters.class 
+})
+@EnableTransactionManagement
+public class SpringBootMultiplasImagensUpload {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultiplasImagensUpload.class, args);
+	}
+}
---
+++ src/test/java/br/com/projeto/multiplasImagens/CrudRestFull1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.multiplasImagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestFull1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
